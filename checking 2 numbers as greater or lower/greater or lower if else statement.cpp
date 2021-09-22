98e4ec510e175d4e372b0f19a1281c1265f0e73a
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2021-09-22 23:55:15 +0530

    If Else
    comparision between two numbers as larger or smaller

---
+++ checking 2 numbers as greater or lower/greater or lower if else statement.cpp
@@ -0,0 +1,21 @@
+//checking the 'a' number is greater or smaller
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter the value of a= ");
+	scanf("%d",&a);
+	
+	printf("Enter the value of b= ");
+	scanf("%d",&b);
+	
+	
+		if( a>b)
+		{
+			printf("a number is greater");
+		}
+		else
+		{
+			printf("a number is samller");
+		}
+}
