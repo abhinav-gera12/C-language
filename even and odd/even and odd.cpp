a0328dba0a1de45206f7cfe96dbc87fb48f7966e
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2021-09-23 23:52:56 +0530

    even or odd

---
+++ even and odd/even and odd.cpp
@@ -0,0 +1,17 @@
+//determine whether the number is even or odd
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the value of a= ");
+	scanf("%d",&a);
+	
+		if( a%2 == 0)
+		{
+			printf("The number is even");
+		}
+		else
+		{
+			printf("The number is odd");
+		}
+}
