4121dec1ccdea3f0cac5a80ed9924598415de10e
Abhinav Gera <63844738+abhinav-gera12@users.noreply.github.com>
2021-09-23 23:53:49 +0530

    simple if statement control flow decision making

---
+++ if statement/if statement.cpp
@@ -0,0 +1,11 @@
+//if statement:- only and only if the statement is true it will execute otherwise it will discarded
+#include<stdio.h>
+main()
+{
+	int high=95, small= 31;
+	//simple statement if, no brackets
+	if(high>small)
+	{
+		printf("Jackpot\n");
+	}
+}
